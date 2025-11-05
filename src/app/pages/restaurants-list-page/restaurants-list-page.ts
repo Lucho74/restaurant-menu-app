7c9285faf3159c0b6563b7090fc286078d680acf
Lucho74 <luchocilenti@gmail.com>
2025-11-05 17:39:54 -0300

    Agrego componentes home-page, login-page, register-page, restaurants-list-page, restaurant-menu-page, header-loyout

---
+++ src/app/pages/restaurants-list-page/restaurants-list-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurants-list-page',
+  imports: [],
+  templateUrl: './restaurants-list-page.html',
+  styleUrl: './restaurants-list-page.scss',
+})
+export class RestaurantsListPage {
+
+}
